1f76a8ee2840340196e18002fd37eadca21b99d6
GarethG <info@gareth-griffiths.co.uk>
2010-12-28 19:47:40 +0000

    more work on getting the fitness value of each genome and indexing that fitness value. dont use fitness in a list, seems to be out of line with using array

--- GA1.py
+++ GA1.py
@@ -7,32 +7,40 @@ import numpy #can be used with matlab?
 import random
 import sys
 
-numpy.set_printoptions(threshold=sys.maxint) #numpy like to print large arrays wierd, supress this
+numpy.set_printoptions(threshold=sys.maxint) #numpy likes to print large arrays wierd, supress this
 
 
 #specify the population size and the genome size - do this later with raw_input
 pop = 50 #population
 gen = 10 #number of bits or genome size
+fitval = []
+
+def pop_array(rows, cols):
+
+	seq = [int(i) for i in range(int(0), int(2))]
+	matrix = []
+
+	for row in range(rows):
+		array = []
+		for col in range(cols):
+			array.append(random.choice(seq))	
+		matrix.append(array)
+		fit = array.count(1)
+		fitval.append(fit)
+		
+		
+	return matrix
+	
+	
+
+a = pop_array(pop,gen)# function call to create an array and populate it
+b = numpy.array(a) #convert the python list array to a numpy array - probably just for printing
+c = fitval.index(10) + 1 #zero referenced
 
 
-def populate_matrix(rows, cols):
-
-    seq = [int(i) for i in range(int(0), int(2))]
-    matrix = []
-
-    for row in range(rows):
-        array = []
-        for col in range(cols):
-            array.append(random.choice(seq))
-        matrix.append(array)
-
-    return matrix
-
-
-
-a = populate_matrix(pop,gen)
-b = numpy.array(a)
-numpy.set_printoptions(threshold=sys.maxint)
+print "Initial Population created"
 print b
+print fitval
+print "the index of 10 fitval genes", c
 
 
