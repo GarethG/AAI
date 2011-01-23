8038508f0dea5577a2f7338b736b1ee2accb9895
GarethG <info@gareth-griffiths.co.uk>
2011-01-23 00:04:57 +0000

    working on line follower, need to make switch statement to turn robot, rather than adding or subtracting from heading which is stupid

--- ass1.py
+++ ass1.py
@@ -9,6 +9,7 @@ import random
 import array
 import sys
 import pprint as pp
+import copy
 
 numpy.set_printoptions(threshold=sys.maxint) #numpy likes to print large arrays wierd, supress this
 
@@ -25,7 +26,7 @@ maze =  [
 	[ 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 ],
 	[ 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 ],
 	[ 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 ],
-	[ 0 , 2 , 1 , 1 , 0 , 0 , 0 , 1 ],
+	[ 0 , 1 , 1 , 1 , 0 , 0 , 0 , 1 ],
 	[ 0 , 0 , 0 , 1 , 1 , 1 , 1 , 1 ],
 	[ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
 	]
@@ -45,9 +46,9 @@ class robot:
 
 #++++++++++++++++++++++init robot++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 def robot_init():
-	robot.x = 2 # the row
-	robot.y = 2 # the col
-	robot.heading = 0
+	robot.x = 1 # the row
+	robot.y = 0 # the col
+	robot.heading = 1 #Start Facing East
 	print "Robot Initialised"
 
 #+++++++++++++++++++++++++++++++++MOVE ROBOT+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -58,25 +59,35 @@ def robot_move(xpos, ypos, robheading):
 
 #++++++++++++++++++++++++++++++++GET SENSORS++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 def robot_getsensors(xpos, ypos, robheading):
-	#robot.sens[1] = 
-	print "getting sensors"
-	print "heading ", robot.heading
+	
+	print "Getting Sensors"
 
-	x = robot.x
-	minusx = robot.x - 1
-	plusx = robot.x + 1
+	x = xpos
+	minusx = xpos - 1
+	plusx = xpos + 1
 	
-	y = robot.y
-	minusy = robot.y - 1
-	plusy = robot.y + 1
-	
-	#+++++++++++NORTH HEADING+++++++++++++++++++++++++++++++++++
+	y = ypos
+	minusy = ypos - 1
+	plusy = ypos + 1
+
+	#if the robot reaches the end of the maze it's sensors should wrap around so
+	#check to see if robot is at a border of a maze
+	if robot.x == 0:
+		minusx = 7
+	if robot.x == 7:
+		plusx = 0
+	if robot.y == 0:
+		minusy = 7		
+	if robot.y == 7:
+		plusy = 0	
+
+	#+++++++++++NORTH HEADING+++++++++++++++++++++++++++++++++++	
 	if robot.heading == 0: #North
-		robot.sens[0] = nmaze[x,	minusy	]
-		robot.sens[1] = nmaze[minusx,	minusy	]
-		robot.sens[2] = nmaze[minusx,	y	]
-		robot.sens[3] = nmaze[minusx,	plusy	]
-		robot.sens[4] = nmaze[x,	plusy 	]
+		robot.sens[0] = nmaze[x,	minusy	]	#Left Sensor
+		robot.sens[1] = nmaze[minusx,	minusy	]	#Left Diagonal Sensor
+		robot.sens[2] = nmaze[minusx,	y	]	#Forward Sensor
+		robot.sens[3] = nmaze[minusx,	plusy	]	#Right Diagonal Sensor
+		robot.sens[4] = nmaze[x,	plusy 	]	#Right Sensor Sensor
 	#++++++++++EAST HEADING+++++++++++++++++++++++++++++++++++++
 	if robot.heading == 1: # East
 		robot.sens[0] = nmaze[minusx,	y	]
@@ -99,6 +110,55 @@ def robot_getsensors(xpos, ypos, robheading):
 		robot.sens[3] = nmaze[minusx,	minusy	]
 		robot.sens[4] = nmaze[minusx,	y	]
 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#+++++++++++++++++++LINE FOLLOWER++++++++++++++++++++++++++++++++++++++++++++++++
+def line_follow():
+	if robot.x == 0:
+		robot.x = 7
+	if robot.x == 7:
+		robot.x = 0
+	if robot.y == 0:
+		robot.y = 7		
+	if robot.y == 7:
+		robot.y = 0	
+
+	if robot.sens[0] == 1:
+		robot.heading = robot.heading - 1	#Turn Left
+	elif robot.sens[2] == 1:
+		#Move Forward
+		print "move forward", robot.x, robot.y
+		if robot.heading == 0: #heading north
+			robot.y = robot.y - 1
+			
+		if robot.heading == 1: #heading east
+			robot.x = robot.x + 1
+
+		if robot.heading == 2: #heading south
+			robot.y = robot.y + 1
+
+		if robot.heading == 3:
+			robot.x = robot.x - 1
+  
+	elif robot.sens[4] == 1:
+		robot.heading = robot.heading + 1	#Turn Right
+	else:#Move Forward
+		if robot.heading == 0: #heading north
+			robot.y = robot.y - 1
+			
+		if robot.heading == 1: #heading east
+			robot.x = robot.x + 1
+
+		if robot.heading == 2: #heading south
+			robot.y = robot.y + 1
+
+		if robot.heading == 3:
+			robot.x = robot.x - 1
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+def draw_map():
+	pass
+
 print "Create Maze"
 pp.pprint(maze)
 
@@ -109,7 +169,30 @@ print nmaze
 
 robot_init() #initialise the robot
 
-robot_getsensors(0,1,0)
+print "Getting first position"
+robot_getsensors(robot.x,robot.y,robot.heading)
+
+
+for life in range(25):
+	line_follow()
+	robot_getsensors(robot.x,robot.y,robot.heading)
+	print "Sensors ", robot.sens
+	print "Heading" , robot.heading
+	print "robot x pos ", robot.x
+	print "robot y pos ", robot.y
+	maze2 = copy.deepcopy(maze)
+	maze2[robot.x][robot.y] = 'R'
+	pp.pprint(maze2)
+	
+print"check the old maze is still intact"
+pp.pprint(maze)
+
+#maze2[2][2] = 'R'
+#pp.pprint (maze2)
+
+#draw_map()
+
+
 #+++++++++++++++++++++++++++workout the sensors++++++++++++++++++++++++++++++++++
 
 #a = nmaze[robot.y,robot.x]
