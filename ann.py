6d6485239f2a1971092545592a3b7a450e0906f7
GarethG <info@gareth-griffiths.co.uk>
2011-03-23 17:07:29 +0000

    ann working but not solved a maze yet

--- ann.py
+++ ann.py
@@ -19,6 +19,13 @@ import pipes
 random.seed()
 numpy.set_printoptions(threshold=sys.maxint) #numpy likes to print large arrays wierd, supress this
 
+
+fitness = open('./data/annfitness.txt','a') #opens bestfit file, arg 'a' opens the file for appending data
+def Filewrite_Fitness():
+	fit = Robot.fitval
+	fit = str(fit)	
+	fitness.write (fit)
+	fitness.write(' \n')	
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #+++++++++++++ ORIGINAL MAZE - DONT WRITE TO THIS++++++++++++++++++++++++++++++++++++++++++++++++++
 Master_Maze =  [
@@ -50,28 +57,33 @@ class Robot:
  	format_head = 0 	#keep the display of heading separate to the actual heading, its only for show
 	health = 25  		#Robot can only move 25 places so every move it looses a health point  
 	fitval = 0 		#Robot Fitness
+	oldfit = 0	
 	sens = [0,0,0,0,0] 	#[left, leftd, fwd, rightd, right]
 	dsens = 0 		#Denary Sensor Values
 	cwarn = 0 		#Crash Warning Flag
 	genome = []
+	finflag = 0
 
 class Neuron1:
 	weight = []
+	oldw = []
 	val = 0
 	outweight = 0
 		
 class Neuron2:
 	weight = []
+	oldfit = 0
 	val = 0
 	outweight = 0
 
 class Neuron3:
 	weight = []
+	oldfit = 0
 	val = 0
 	outweight = 0 
 
 class OutNeu1:
-	weight = []
+	weight = 0
 	val = 0
 
 class OutNeu2:
@@ -110,7 +122,7 @@ def Init_Neurons():
 	print "Neuron 1",Neuron2.weight
 	print "Neuron 1",Neuron3.weight
 
-	Neuron1.outweight = random.random()
+	OutNeu1.weight = random.random()
 	Neuron2.outweight = random.random()
 	Neuron3.outweight = random.random()		
 
@@ -268,33 +280,187 @@ def Get_Sensors():
 	#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++	
 	#print "Got Sensors"
 
+def Robot_Action(val):
+	action = val
+	if action == 0:
+		print "Action called - Do Nothing"	
+	elif action == 1:
+		print "Action called - Turn Right"
+	elif action == 2:
+		print "Action called - Turn Left"
+	elif action == 3:
+		print "Action called - Go Forward"
+	
+	#now we have an action selected, drive the robot
+	#+++++++++++++++++STATE MACHINE ACTION SELECTOR++++++++++++++++++++++++++++++++++++++++++++
+	if action == 0:
+		Robot.cwarn = 0 #do nothing
+	elif action == 1: #Turn Right
+		Robot.cwarn = 0 #now that you've turned reset the crash flag
+		if Robot.heading == 0: 			#Heading at North, make your heading East
+			Robot.heading = 1	
+		elif Robot.heading == 1: 		#heading at East, make your heading South						
+			Robot.heading = 2 			
+		elif Robot.heading == 2: 		#heading at South, make your heading West
+			Robot.heading = 3
+		elif Robot.heading == 3: 		#heading at West, make your heading North
+			Robot.heading = 0			
+	elif action == 2: #Turn Left
+		Robot.cwarn = 0 #now that you've turned reset the crash flag
+		if Robot.heading == 0: 			#Heading at North, make your heading West
+			Robot.heading = 3
+		elif Robot.heading == 1: 		#Heading at East, make your heading North
+			Robot.heading = 0
+		elif Robot.heading == 2: 		#Heading at South, make your heading East
+			Robot.heading = 1
+		elif Robot.heading == 3:		#Heading at West, make your heading South
+			Robot.heading = 2
 
+	elif action == 3: #Go Forward 
+		if Robot.heading == 0: #heading north
+			if Robot.cwarn == 1: #crash warning raised
+				#print "wrapping robot at X Co-ord", Robot.x
+				Robot.x = Robot.x + 7 #your at a border dont drive into that, so wrap to top of maze
+				#time.sleep(5)				
+				Robot.cwarn = 0 #reset crash flag
+			elif Robot.cwarn == 0: #was just an else 
+				Robot.x = Robot.x - 1						     
+
+		elif Robot.heading == 1: #heading east								
+			if Robot.cwarn == 1:
+				#print "wrapping robot at X Co-ord", Robot.x
+				Robot.y = Robot.y - 7
+				#time.sleep(5)
+				Robot.cwarn = 0			
+			elif Robot.cwarn == 0: #was just an else
+				Robot.y = Robot.y + 1							
+		
+		elif Robot.heading == 2: #heading south
+			if Robot.cwarn == 1:
+				#print "wrapping robot at X Co-ord", Robot.x
+				Robot.x = Robot.x - 7
+				#time.sleep(5)				
+				Robot.cwarn = 0							
+			elif Robot.cwarn == 0: #was just an else
+				Robot.x = Robot.x + 1		
+							
+		elif Robot.heading == 3: #heading west
+			if Robot.cwarn == 1:
+				#print "wrapping robot at X Co-ord", Robot.x
+				#time.sleep(5)				
+				Robot.cwarn = 0
+				Robot.y = Robot.y + 7							
+			elif Robot.cwarn == 0: #was just an else
+				Robot.y = Robot.y - 1
+		
+		Robot_Fitness()	
+		Robot.cwarn = 0
+		Del_Tile()#delete the tile your on so you dont count its fitness more than once
+
+#------------------Neural Network--------------------------------------------------------------------NEURAL NETWORK
+#
+#
+#
+def ANN():
+	for life in range(Robot.health):
+		Get_Sensors()
+		Neuron1.val = (Robot.sens[0] * Neuron1.weight[0]) + (Robot.sens[2] * Neuron1.weight[1]) + (Robot.sens[4] * Neuron1.weight[2])
+		Neuron2.val = (Robot.sens[0] * Neuron2.weight[0]) + (Robot.sens[2] * Neuron2.weight[1]) + (Robot.sens[4] * Neuron2.weight[2])
+		Neuron3.val = (Robot.sens[0] * Neuron3.weight[0]) + (Robot.sens[2] * Neuron3.weight[1]) + (Robot.sens[4] * Neuron3.weight[2])
+	
+		print"1 = ", Neuron1.val
+		print"2 = ", Neuron2.val
+		print"3 = ", Neuron3.val
+	
+		#if a neuron is over the firerate then output its value to the output neurons
+	
+		OutNeu1.val = (Neuron1.val * OutNeu1.weight) + (Neuron2.val * OutNeu1.weight) + (Neuron3.val * OutNeu1.weight)
+		OutNeu2.val = (Neuron1.val * OutNeu2.weight) + (Neuron2.val * OutNeu2.weight) + (Neuron3.val * OutNeu2.weight)
+	
+		# if the value of the output neuron is above the firerate then normalise to 1 or 0 and output to drive
+	
+	
+		if OutNeu1.val > 1:
+			OutNeu1.val = 1
+		elif OutNeu1.val < 1:
+			OutNeu1.val = 0
+	
+		if OutNeu2.val > 1:
+			OutNeu2.val = 1
+		if OutNeu2.val < 1:
+			OutNeu2.val = 0
+	
+		if OutNeu1.val == 0 and OutNeu2.val == 0:
+			Robot_Action(0)
+			Format_Heading()
+		elif OutNeu1.val == 0 and OutNeu2.val == 1:
+			Robot_Action(1)
+			Format_Heading()
+		elif OutNeu1.val == 1 and OutNeu2.val == 0:
+			Robot_Action(2)
+			Format_Heading()
+		elif OutNeu1.val == 1 and OutNeu2.val == 1:
+			Robot_Action(3)
+			Format_Heading()
+	
+		maze2 = copy.deepcopy(Maze.maze) #this is just for formating
+		maze2[Robot.x][Robot.y] = Robot.format_head #format heading
+		pp.pprint(maze2)
+		print "Robot Fitness ", Robot.fitval
+		time.sleep(0)
+		Filewrite_Fitness()
+		
+	if Robot.fitval == 19:
+		Robot.finflag = 1
+	else:
+		Robot.finflag = 0
+
+def Mutation(val):
+	a = 0
+	b = random.randint(0,1)
+	if b == 0:
+		a = random.random()
+		a = a/10	
+		outval = val + a
+	elif b == 1:
+		a = random.random()
+		a = a * -1
+		a = a/10
+		outval = val + a 
+		
+
+
+	return outval
 #------------------------MAIN----------------------------------------------------------------------MAIN
 Init_Maze()
 Init_Robot()
 Init_Neurons()
 
-Get_Sensors()
-Neuron1.val = (Robot.sens[0] * Neuron1.weight[0]) + (Robot.sens[2] * Neuron1.weight[1]) + (Robot.sens[4] * Neuron1.weight[2])
-Neuron2.val = (Robot.sens[0] * Neuron2.weight[0]) + (Robot.sens[2] * Neuron2.weight[1]) + (Robot.sens[4] * Neuron2.weight[2])
-Neuron3.val = (Robot.sens[0] * Neuron3.weight[0]) + (Robot.sens[2] * Neuron3.weight[1]) + (Robot.sens[4] * Neuron3.weight[2])
+while Robot.finflag == 0:
+	Init_Maze()
+	Init_Robot()	
+	ANN()
+	Robot.oldfit = Robot.fitval
+	Neuron1.oldw = Neuron1.weight
+	Neuron2.oldw = Neuron2.weight
+	Neuron3.oldw = Neuron3.weight
+	if Robot.oldfit > Robot.fitval:
+		#old fitness is better throw out the new weights, they suck
+		Neuron1.weight = Neuron1.oldw
+		Neuron1.weight = Neuron1.oldw
+		Neuron1.weight = Neuron1.oldw
 
-print"1 = ", Neuron1.val
-print"2 = ", Neuron2.val
-print"3 = ", Neuron3.val
+	elif Robot.fitval < Robot.oldfit:
+		pass	#keep the new weights, they rule
+	
+	#Mutation
+	for i in range(0,3):
+		print "mutating"
+		print "val" , Neuron1.weight[i]
+		Neuron1.weight[i] = Mutation(Neuron1.weight[i])
+		print "outval" , Neuron1.weight[i]
 
-#if a neuron is over the firerate then output its value to the output neurons
-
-OutNeu1.val = (Neuron1.val * OutNeu1.weight[0])# + (Neuron2.val * OutNeu1.weight[1]) + (Neuron3.val * OutNeu1.weight[2])
-
-# if the value of the output neuron is above the firerate then normalise to 1 or 0 and output to drive
-
-
-
-
-
-
-
+	
 
 
 
